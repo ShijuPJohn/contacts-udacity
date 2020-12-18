e0a0e86fcc0f2cfbb74eff155be4c6bf2a7429df
ShijuPJohn <shijupjohn4u@gmail.com>
2020-12-18 16:28:41 +0530

    complete create new contact functionality, routes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,54 @@
 import React, {Component} from 'react';
 import ListContacts from "./ListContacts";
+import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from "./CreateContact";
+import {Route} from "react-router-dom";
 
 
 class App extends Component {
+
     state = {
-        contacts: [
-            {
-                "id": "karen",
-                "name": "Karen Isgrigg",
-                "handle": "karen_isgrigg",
-                "avatarURL": "http://localhost:5001/karen.jpg"
-            },
-            {
-                "id": "richard",
-                "name": "Richard Kalehoff",
-                "handle": "richardkalehoff",
-                "avatarURL": "http://localhost:5001/richard.jpg"
-            },
-            {
-                "id": "tyler",
-                "name": "Tyler McGinnis",
-                "handle": "tylermcginnis",
-                "avatarURL": "http://localhost:5001/tyler.jpg"
-            }
-        ]
+        contacts: []
     }
+
+    componentDidMount() {
+        ContactsAPI.getAll().then((contacts) => {
+            this.setState((prevState) => ({
+                contacts: contacts
+            }))
+        })
+    }
+
     removeContact = (contact) => {
+        ContactsAPI.remove(contact)
         this.setState(prevState => ({
             contacts: prevState.contacts.filter(c => {
                 return c.id !== contact.id
             })
         }))
+
+    }
+
+    createContactFunction(data) {
+        ContactsAPI.create(data).then((contact) => {
+            this.setState((prevState) => ({
+                contacts: prevState.contacts.concat([contact])
+            }))
+        })
     }
 
     render() {
         return (
             <div>
-                <ListContacts onDeleteFunction={this.removeContact} contacts={this.state.contacts}/>
+                <Route path={'/create'} exact
+                       render={({history}) => <CreateContact onCreateContact={(contact) => {
+                           this.createContactFunction(contact)
+                           history.push('/')
+                       }}/>}/>
+                <Route path={'/'} exact render={() => (
+                    <ListContacts onDeleteFunction={this.removeContact} contacts={this.state.contacts}/>
+                )}/>
+
             </div>
         );
     }
---
+++ src/CreateContact.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react'
+import {Link} from "react-router-dom";
+import ImageInput from "./ImageInput";
+import serializeForm from 'form-serialize';
+
+class CreateContact extends Component {
+    handleSubmit(event) {
+        event.preventDefault()
+        const values = serializeForm(event.target, {
+            hash: true
+        })
+        console.log(values)
+        if (this.props.onCreateContact) {
+            this.props.onCreateContact(values)
+        }
+    }
+
+    render() {
+        return <div>
+            <Link to={'/'} className={'close-create-contact'}>
+                Close
+            </Link>
+            <form onSubmit={this.handleSubmit.bind(this)} className={'create-contact-form'}>
+                <ImageInput className={'create-contact-avatar-input'}
+                            name={'avatarURL'}
+                            maxHeight={64}/>
+                <div className={'create-contact-details'}>
+                    <input type={'text'} name={'name'} placeholder={'Name'}/>
+                    <input type={'text'} name={'handle'} placeholder={'Handle'}/>
+                    <button>Save</button>
+                </div>
+            </form>
+        </div>
+    }
+}
+
+export default CreateContact
